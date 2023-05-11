7e3e315cde5f4406b44a129213cf98452aacefad
bigracer01 <63704964+bigracer01@users.noreply.github.com>
2023-05-11 15:30:50 +0200

    connection dash à ElasticSearch + refacto

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
 from jupyter_dash import JupyterDash
+from connect_Elastic import  connect_elastic_server
+from request_elasticsearch import get_count_article_range2
 
 app = JupyterDash(__name__)
 
@@ -15,11 +17,14 @@ app.layout = html.Div([
 ])
 
 
+es = connect_elastic_server()
 
-
-
-
-df = pd.read_csv('data_brutes/data_articles/nyt.csv')
+def update_date(filter):
+    date, count = get_count_article_range2(es, filter)
+    d = {'Date': date, 'Count': count}
+    df = pd.DataFrame(d)
+    print(df)
+    return df
 
 
 # definir une fonction de rappel pour mettre à jour le graphique en fonction du mot de recherche
@@ -27,22 +32,18 @@ df = pd.read_csv('data_brutes/data_articles/nyt.csv')
     dash.dependencies.Output('graphique', 'figure'),
     [dash.dependencies.Input('mot-recherche', 'value')])
 def update_graph(mot_recherche):
-    # filtrer les articles qui contiennent le mot recherché dans leur keyword
-    df_filtre = df[df['keywords'].str.contains(mot_recherche, case=False)]
-
-    # convertir la colonne pub_date en datetime et extraire la date
-    df_filtre['date'] = pd.to_datetime(df_filtre['pub_date']).dt.date
-
-    # regrouper les articles par date et compter le nombre d'articles pour chaque date
-    df_group = df_filtre.groupby('date').size().reset_index(name='count')
+    data = update_date(mot_recherche)
 
     # graphique avec plotly
-    fig = px.line(df_group, x='date', y='count',
+    fig = px.line(data, x='Date', y='Count',
                   title='Nombre d\'articles par jour contenant le mot "{}"'.format(mot_recherche))
 
     return fig
+
+
 def create_dashboard():
     app.run_server(mode='inline')
+
 
 # lancé Dash avec Jup
 
--- request_elasticsearch.py
+++ request_elasticsearch.py
@@ -38,11 +38,43 @@ def get_count_article_range(elasticsearch, request):
 print(get_count_article_range(es, req))
 
 
-def get_count_article_range2(elasticsearch, request,param):
-
+def get_count_article_range2(elasticsearch,param):
+    name = param
     date = []
     count = []
-    result_request = elasticsearch.search(index='article', body=request, filter_path=["aggregations.group_by_date.buckets"])
+    req = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "match": {
+                            "Titres": name
+                        }
+                    }
+                ],
+                "filter": [
+                    {
+                        "range": {
+                            "pub_date": {
+                                "gte": "2020-01-01",
+                                "lte": "2020-01-25"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        "aggs": {
+            "group_by_date": {
+                "date_histogram": {
+                    "field": "pub_date",
+                    "calendar_interval": "day",
+                    "format": "yyyy-MM-dd"
+                }
+            }
+        }
+    }
+    result_request = elasticsearch.search(index='article', body=req, filter_path=["aggregations.group_by_date.buckets"])
     for i in result_request['aggregations']["group_by_date"]["buckets"] :
         print(i['key_as_string'])
         date.append(i['key_as_string'])
