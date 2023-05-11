bd87d67465bf438496db2808eb55708e70bc8372
bigracer01 <63704964+bigracer01@users.noreply.github.com>
2023-05-11 14:37:36 +0200

    fix commit

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-import os
 from articles_functions import create_tab_article_to_csv
-import books_functions
 from connect_Elastic import connect_elastic_server, create_database
 import json
+from dashboard import create_dashboard,update_graph
+from request_elasticsearch import get_count_article_range
 
 with open("resources/mapping.json") as json_data_file:
     mapping = json.load(json_data_file)
 
 
+
+
+
+es = connect_elastic_server()
 def create_bdd():
     # books_functions.create_tab_list_names()
     # books_functions.create_tab_books()
     create_tab_article_to_csv()
-    es = connect_elastic_server()
-    create_database(es, "data_brutes/data_articles/nyt.csv","article", mapping)
+    try :
+        create_database(es, "data_brutes/data_articles/nyt.csv","article", mapping)
+    except :
+        print("Il existe déjà une base de donnée")
 
     # stop after 1 or 2 "response ok"
     # si l'algo se stop, une boucle de create_tab_books s'est arreté, verifier alors si une table est apparu dans les bdd
     # si oui alors exceuter juste create_tab_article()
 
+def connect_dash():
+
+    create_dashboard()
+    update_graph()
 
 if __name__ == "__main__":
     # if len(os.listdir('data_brutes/data_books')) == 0:
-    create_bdd()
+    ##create_bdd()
+    connect_dash()
