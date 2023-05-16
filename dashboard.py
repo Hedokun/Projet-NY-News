5d74565f50c76a741e8aeb3a6a1c5322b7cbd2d3
Edouard <loiseau.edouard@gmail.com>
2023-05-16 08:50:41 +0200

    modif Dash
    Bouton à gauche
    Ajout de graph en onglet
    Modif du tableau (sur html.Table)
    Ajout de Logo
    couleur légerment modifié
    Rajout d'un Footer

--- dashboard.py
+++ dashboard.py
@@ -25,77 +25,89 @@ liens = ["https://www.nytimes.com/2019/12/31/us/texas-church-shooting-white-sett
 titre = ['Battling a Demon: Drifter Sought Help Before Texas Church Shooting', 'Protect Veterans From Fraud', 'F.D.A. Plans to Ban Most E-Cigarette Flavors but Menthol', 'It’s Green and Slimy', 'Corrections: Jan. 1, 2020', 'Quotation of the Day: Ex-SEAL Now Pitching Products and President', 'Hospitals and Schools Are Being Bombed in Syria. A U.N. Inquiry Is Limited. We Took a Deeper Look.', 'Hong Kong Protesters Return to Streets as New Year Begins', 'Kevin Spacey Accuser’s Estate Drops Sexual Assault Lawsuit', 'Dizzying Day for Trump Caps a Year Full of Them']
 categories = ['U.S.', 'Opinion', 'Health', 'Crosswords & Games', 'Corrections', 'Today’s Paper', 'World', 'Science', 'Arts', 'Business Day', 'Magazine', 'Books', 'Well', 'Travel', 'Technology', 'New York', 'Movies', 'Real Estate', 'Sports', 'Theater', 'Food', 'Style', 'Parenting', 'Briefing', 'Climate', 'Smarter Living', 'Obituaries', 'The Learning Network', 'Reader Center', 'Podcasts', 'T Magazine', 'The Upshot', 'Fashion & Style', 'Your Money', 'Neediest Cases', 'Admin', 'T Brand', 'Video', 'The Weekly', 'Education', 'Automobiles', 'Sunday Review', 'Multimedia/Photos']
 
-
+logo = html.Img(src="https://i0.wp.com/datascientest.com/wp-content/uploads/2020/08/new-logo.png", height="70px", 
+                 className="position-absolute top-0 end-60")   
 # titre
 header = dbc.NavbarSimple(
-    children=[
+    children = [
         dbc.NavItem(dbc.NavLink(href="#")),
     ],
-    brand=html.Span("New-York Time Dashboard", className="font-roboto", style={'font-size': '36px', 'text-align': 'center',"font-weight": "bold"}),
+    brand = html.Div([
+        html.Span(" New-York Time Dashboard", className="font-roboto", style={'font-size': '36px', 'text-align': 'center',"font-weight": "bold"})
+        ]),
     brand_href="#",
-    color="#0D75FA",
+    color="#472ADE",
     dark=True,
     links_left = True,
     class_name='mb-3'
 )
+footer = html.Footer(
+    dbc.Container(
+        dbc.Row(
+            dbc.Col(
+                html.P("Ce projet c'est fait dans le cadre de la formation Datascientest par Alexis Peron, Edouard Loiseau, Louis Petat Lenoir."),
+                style={'text-align': 'center'}
+            )
+        ),
+        fluid=True
+    ),
+    className="footer py-3 mt-5 bg-light"
+)
 
+ 
 # bouton: mot de recherche, sélection catégorie, date
 selector =dbc.Row([
             dbc.Card(             
                 dbc.Row(children = [
-                    dbc.Col(dbc.Input(id='mot-recherche',placeholder="Entrez le mot à rechercher", type="text"),width=2,className="mb-2 rounded"),
-                    dbc.Col(dcc.Dropdown(id='dropdown-categories', options=[{'label': category, 'value': category} for category in categories],style={'width': '60%'} , placeholder="Sélectionnez une catégorie")),
-                    dbc.Col(dcc.DatePickerRange(id='my-date-picker-range', min_date_allowed=date(1995, 8, 5), max_date_allowed=date(2023, 5, 11),initial_visible_month=date(2017, 8, 5), end_date=date(2017, 8, 25),className= "rounded"))
+                    dbc.Col(dbc.Input
+                            (id='mot-recherche',placeholder="Entrez le mot à rechercher", type="text"),width=12,className="mb-2 rounded"),
+                    dbc.Col(dcc.Dropdown
+                            (id='dropdown-categories', options=[{'label': category, 'value': category} for category in categories],style={'width': '100%'} , placeholder="Sélectionnez une catégorie")),
+                    dbc.Col(dcc.DatePickerRange
+                            (id='my-date-picker-range', min_date_allowed=date(1995, 8, 5), max_date_allowed=date(2023, 5, 11),initial_visible_month=date(2017, 8, 5), end_date=date(2017, 8, 25),className= "rounded"))
                     
-        ]),className="rounded shadow-sm p-4"),  
+        ],className="flex-column"),className="rounded shadow-sm p-4"),  
     ],className="p-3")
 
-#graph 1 categories
-graph_1=dbc.Col(
-        dbc.Card(
-            dbc.CardBody(children=[dcc.Graph(id='graphique-categories')]),
-            className="shadow bg-white rounded"
-        ),
-        width=7,
-        className='p-3')
+
         
 #tableau 1 titres et liens
-tab_1 = dbc.Col(
-    dash_table.DataTable(
-        columns=[
-            {"name": "Titres", "id": "Titres"},
-            {"name": "Liens", "id": "Liens", "type": "text",
-             "presentation": "markdown"},
+tab_Titres = dbc.Col(
+    dbc.Table(
+        [
+            html.Thead(
+                html.Tr([html.Th("Latest News")]),
+                style={"background-color": "#f8f9fa"}
+            ),
+            html.Tbody([
+                    html.Tr([
+                            html.Td(html.A(t,href=lien,target="_blank",)),
+                            ])
+                    for t, lien in zip(titre, liens)
+                    ]),
         ],
-        data=[
-            {"Titres": t, "Liens": f"[{lien}]({lien})"} for t, lien in zip(titre, liens)
-            #pour que ce soit le titre qui devienne cliquable ont peut remplacer part:
-            #{"Titres": t, "Liens": f"[{t}]({lien})"} for t, lien in zip(titre, liens)
-        ],
-        style_table={'overflowX': 'auto'},
-        style_cell={
-            'minWidth': '0px', 'maxWidth': '180px',
-            'whiteSpace': 'normal',
-            'textAlign': 'left',
-            'fontFamily': 'Roboto'
-        },
-        style_data={
-            'whiteSpace': 'normal',
-            'height': 'auto',
-            'lineHeight': '15px'
-        },
-        page_size=10
+        bordered=True,
+        responsive=True,
+        hover=True,
+        striped=True,
     ),
-    width=4
+)
+#graph 1 categories
+graph_1 =  dbc.Tab(
+    label="Graphique 1",
+    children=[
+        dbc.Card(
+            dbc.CardBody(dcc.Graph(id='graphique-1')),className="shadow bg-white rounded")
+    ]
 )
 #graph 2 abstract
-graph_2 = dbc.Col(
-                dbc.Card(
-                    dbc.CardBody(children=[dcc.Graph(id='graphique-abstract')]),
-                    className="shadow bg-white rounded"
-                ),
-                width=7)
-
+graph_2 = dbc.Tab(
+    label="Graphique 2",
+    children=[
+        dbc.Card(
+            dbc.CardBody(dcc.Graph(id='graphique-2')),className="shadow bg-white rounded")
+    ]
+)
 #tableau 2 Titres
 tableau_2 = dbc.Col(
                 html.Table([
@@ -105,21 +117,33 @@ tableau_2 = dbc.Col(
                 , width=4)
 
 #tableau 1 liens
-tableau__liens_2 = dbc.Col(
+tableau_liens_2 = dbc.Col(
                 html.Table([
                     html.Thead(html.Tr([html.Th("Liens")]), style={'background-color': '#f8f9fa'}),
                     html.Tbody([html.Tr([html.Td(html.A(href=lien, children=lien))]) for lien in liens])
                 ], className="table")
                 , width=4)
 
+tabs = dbc.Tabs(
+    [graph_1, graph_2],
+    className="mb-3"
+)
 
 # créer une mise en page
-app.layout = html.Div(children=[    
+app.layout = html.Div(children=[  
         header,
-        selector,
-        dbc.Row([graph_1,tab_1], className="mb-3"),
-        dbc.Row([graph_2,tableau_2], className="p-3")
-    
+        logo, 
+        #selector,
+        dbc.Row([
+        #dbc.Row([tabs,tab_1], className="mb-3"),
+        #dbc.Row([graph_2,tableau_2], className="p-3")
+        #dbc.Row([
+        dbc.Col(selector,width=2, className="p-3"),        
+        dbc.Col(tabs, className="p-3"),
+        dbc.Col(tab_Titres,width=3, className="p-3"),
+        footer
+        ])
+
 ])
 
 
@@ -131,6 +155,7 @@ except :
     es = 0
 
 def update_date(filter):
+
     try :
         date, count = get_count_article_range2(es, filter)
         d = {'Date': date, 'Count': count}
@@ -161,6 +186,8 @@ def display_category(selected_category):
 
 
 def update_graph(mot_recherche):
+    if mot_recherche== None :
+        mot_recherche = "France"
     data = update_date(mot_recherche)
 
 
